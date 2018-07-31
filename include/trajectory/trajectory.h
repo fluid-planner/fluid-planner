60b0833e9e08a950a130484373b87d16f2ab8950
Xuning Yang <xuning.yang@tri.global>
2018-07-31 11:01:00 -0400

    Add trajectory.h and trajectory.cc, which contains the trajectory class and model that is used to generate trajectories from parameters.

---
+++ include/trajectory/trajectory.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+#include <trajectory/state.h>
+#include <utils/conversion_utils.h>
+
+// Defines a collection of classes associated with trajectory definitions.
+namespace trajectory {
+
+// Defines a Trajectory class, which represents an m-segment trajectory
+// parameterized by linear velocity `v` and angular velocity `omega`.
+template <class T> class Trajectory {
+public:
+  typedef Eigen::Matrix<T, Eigen::Dynamic, 1> VecXt;
+  typedef Eigen::Matrix<T, 3, Eigen::Dynamic> MatXt;
+  typedef Eigen::Matrix<T, 3, 1> Vec3t;
+
+  // Constructor parameters:
+  //  vels: an m x 1 vector containing linear velocities for each segment
+  //  omegas: an m x 1 vector containing angular velocities for each segment
+  // OR:
+  //  Z: an 2m x 1 vector containing [vels, omegas]
+  // AND:
+  //  seg_duration: a float denoting the duration of each segment (in seconds).
+  Trajectory(const std::vector<T> &vels, const std::vector<T> &omegas,
+             const float seg_duration);
+  Trajectory(const VecXt &vels, const VecXt &omegas, const float seg_duration);
+  Trajectory(const VecXt &Z, const float seg_duration);
+  ~Trajectory();
+
+  // Sets the Rotation and Translation fields that transforms the trajectory
+  // from local to world frame.
+  void SetTransform(const Eigen::Transform<T, 3, 3> &R_wb, const Vec3t &T_wb);
+
+  // Returns the Z representation of this trajectory; i.e., a 2m x 1 vector that
+  // contains the parameters to the trajectory [vels, omegas].
+  VecXt Z() const;
+
+  // Returns a discretized path representation of this trajectory as a vector of
+  // States, discretized according to `dt`.
+  std::vector<State<T>> GetWaypointsWorld(const float dt);
+  std::vector<State<T>> GetWaypointsLocal(const float dt);
+
+  // Outputs a discretized path representation of this trajectory as two Eigen
+  // Matrices, `pos` and `vel`.
+  void GetWaypointsWorld(const float dt, MatXt *pos, MatXt *vel);
+  void GetWaypointsLocal(const float dt, MatXt *pos, MatXt *vel);
+
+private:
+
+  int num_seg_;                     // Number of segments, m.
+  int num_param_;                   // Number of parameters, which should be 2m.
+
+  VecXt vels_;                      // Linear velocity parameters.
+  VecXt omegas_;                    // Angular velocity parameters.
+  float seg_duration_;              // Duration of each segment.
+
+  bool is_transform_init_ = false;  // A flag indicating if R_wb_ and T_wb_ are
+                                    // initialized.
+  Eigen::Transform<T, 3, 3> R_wb_;  // Rotation from local to world frame.
+  Vec3t T_wb_;                      // Translation from local to world frame.
+
+  // Dubins model parameters:
+  //  v:        linear velocity.
+  //  omega:    angular velocity.
+  //  t:        evaluation time.
+  //  pos_prev: previous position.
+  // Output:
+  //  A 3x1 vector of the position x.
+  Vec3t Dubins(const T v, const T omega, const float t, const Vec3t &pos_prev);
+};
+
+// Declare the explicit instantiations; i.e., the allowed implementations of the
+// Trajectory class. You will only be able to use Trajectory with float or
+// double, but time in all of these instantiations will be represented by a float.
+template class Trajectory<float>;
+template class Trajectory<double>;
+
+} // namespace trajectory
---
+++ src/trajectory/trajectory.cc
@@ -0,0 +1,159 @@
+#include <trajectory/trajectory.h>
+
+namespace cu = conversion_utils;
+
+namespace trajectory {
+
+template <class T> Trajectory<T>::~Trajectory() {}
+
+template <class T>
+Trajectory<T>::Trajectory(const VecXt &vels, const VecXt &omegas,
+                          const float seg_duration)
+    : vels_(vels), omegas_(omegas), seg_duration_(seg_duration) {
+  // Check that the parameters vels and omegas have the correct dimensions.
+  assert(vels.size() != 0);
+  assert(omegas.size() != 0);
+  assert(vels.size() == omegas.size());
+
+  num_seg_ = vels.size();
+}
+
+template <class T>
+Trajectory<T>::Trajectory(const std::vector<T> &vels,
+                          const std::vector<T> &omegas,
+                          const float seg_duration) {
+  // Check that the parameters vels and omegas has the correct dimensions.
+  assert(vels.size() != 0);
+  assert(omegas.size() != 0);
+  assert(vels.size() == omegas.size());
+
+  num_seg_ = vels.size();
+  seg_duration_ = seg_duration;
+
+  vels_ = cu::Vec2Eigen<T>(vels);
+  omegas_ = cu::Vec2Eigen<T>(omegas);
+}
+
+template <class T>
+Trajectory<T>::Trajectory(const Trajectory<T>::VecXt &Z,
+                          const float seg_duration) {
+  // Check that the parameter input Z = [vels, omegas] is of correct size.
+  assert(Z.size() != 0);
+  assert(Z.size() % 2 == 0);
+
+  num_seg_ = Z.size() * 0.5;
+  seg_duration_ = seg_duration;
+
+  vels_ = Z.block(0, 0, num_seg_, 1);
+  omegas_ = Z.block(num_seg_, 0, num_seg_, 1);
+}
+
+template <class T> typename Trajectory<T>::VecXt Trajectory<T>::Z() const {
+  Trajectory<T>::VecXt Z(num_seg_ * 2);
+  Z.block(0, 0, num_seg_, 1) = vels_;
+  Z.block(num_seg_, 0, num_seg_, 1) = omegas_;
+  return Z;
+}
+
+template <class T>
+void Trajectory<T>::SetTransform(const Eigen::Transform<T, 3, 3> &R_wb,
+                                 const Eigen::Matrix<T, 3, 1> &T_wb) {
+  R_wb_ = R_wb;
+  T_wb_ = T_wb;
+  is_transform_init_ = true;
+}
+
+template <class T>
+std::vector<State<T>> Trajectory<T>::GetWaypointsWorld(const float dt) {
+  MatXt pos, vel;
+  GetWaypointsWorld(dt, &pos, &vel);
+
+  std::vector<State<T>> wpts;
+  for (int i = 0; i < pos.cols(); i++) {
+    State<T> state(pos.col(i), vel.col(i), pos(2, i));
+    wpts.push_back(state);
+  }
+  return wpts;
+}
+
+template <class T>
+std::vector<State<T>> Trajectory<T>::GetWaypointsLocal(const float dt) {
+  MatXt pos, vel;
+  GetWaypointsLocal(dt, &pos, &vel);
+
+  std::vector<State<T>> wpts;
+  for (int i = 0; i < pos.cols(); i++) {
+    State<T> state(pos.col(i), vel.col(i), pos(2, i));
+    wpts.push_back(state);
+  }
+  return wpts;
+}
+
+template <class T>
+void Trajectory<T>::GetWaypointsWorld(const float dt, MatXt *pos, MatXt *vel) {
+
+  if (!is_transform_init_) {
+    printf("[Trajectory::GetWaypointsWorld] Warning: transforms not "
+           "initialized. Call SetTransform before calling "
+           "GetWaypointsWorld.\n");
+    return;
+  }
+
+  MatXt pos_b;
+  MatXt vel_b;
+  GetWaypointsLocal(dt, &pos_b, &vel_b);
+
+  size_t n = pos_b.cols();
+  pos->resize(3, n);
+  vel->resize(3, n);
+
+  for (size_t i = 0; i < n; i++) {
+    pos->col(i) = R_wb_ * pos_b.col(i) + T_wb_;
+    vel->col(i) = R_wb_ * vel_b.col(i);
+  }
+}
+
+template <class T>
+void Trajectory<T>::GetWaypointsLocal(const float dt, MatXt *pos, MatXt *vel) {
+  int num_pts = floor(num_seg_ * seg_duration_ / dt);
+  pos->resize(3, num_pts);
+  vel->resize(3, num_pts);
+
+  Trajectory<T>::Vec3t pos_prev = Trajectory<T>::Vec3t::Zero();
+
+  int j = 0;
+  for (int i = 0; i < num_seg_; i++) {
+    T v = vels_(i);
+    T omega = omegas_(i);
+
+    for (float t = 0.0; t < seg_duration_; t += dt) {
+      pos->col(j) = Trajectory<T>::Dubins(v, omega, t, pos_prev);
+      vel->col(j) =
+          Eigen::AngleAxis<T>((*pos)(2, j), Trajectory<T>::Vec3t(0, 0, 1)) *
+          Trajectory<T>::Vec3t(v, 0, 0);
+      j++;
+    }
+    pos_prev = (*pos).col(j - 1);
+  }
+}
+
+template <class T>
+typename Trajectory<T>::Vec3t
+Trajectory<T>::Dubins(const T v, const T omega, const float t,
+                      const Trajectory<T>::Vec3t &pos_prev) {
+  Trajectory<T>::Vec3t x;
+  T theta = pos_prev(2);
+
+  if (std::fabs(omega) < std::numeric_limits<T>::min()) {
+    x = Trajectory<T>::Vec3t(v * t * cos(theta),
+                             v * t * sin(theta),
+                             0.0) + pos_prev;
+  } else {
+    x = Trajectory<T>::Vec3t(v / omega * (sin(theta + t * omega) - sin(theta)),
+                             v / omega * (-cos(theta + t * omega) + cos(theta)),
+                             t * omega) + pos_prev;
+  }
+  return x;
+}
+
+} // namespace trajectory

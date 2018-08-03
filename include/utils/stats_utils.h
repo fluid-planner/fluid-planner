c28cb46f5aabfa84ee5188fb7819542a0a26c1d3
Xuning Yang <xuning.yang@tri.global>
2018-08-03 12:56:38 -0400

    Add covariance and probability density computation

--- include/utils/stats_utils.h
+++ include/utils/stats_utils.h
@@ -7,13 +7,20 @@
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 
+// Defines a collection of statistics utility functions.
 namespace stats_utils {
+
+// Define a sufficiently small value, but
+static constexpr float kSufficientlySmallFloat = 0.00001;
+
+template <typename T> using VecXt = Eigen::Matrix<T, Eigen::Dynamic, 1>;
+template <typename T>
+using MatXt = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
 
 // RangeSample draws k samples of type T from the range [lb, ub), and returns
 // an Eigen::VectorXd object. T is one of { int, float, double }.
 template <typename T>
-Eigen::Matrix<T, Eigen::Dynamic, 1> RangeSample(const T lb, const T ub,
-                                                const int k) {
+VecXt<T> RangeSample(const T lb, const T ub, const int k) {
 
   // Generate uniform real distribution from lb to ub.
   std::random_device rd;
@@ -21,7 +28,7 @@ Eigen::Matrix<T, Eigen::Dynamic, 1> RangeSample(const T lb, const T ub,
   std::uniform_real_distribution<> distribution(lb, ub);
 
   // Sample from range.
-  auto samples = Eigen::Matrix<T, Eigen::Dynamic, 1>(k);
+  auto samples = VecXt<T>(k);
   for (int i = 0; i < k; i++) {
     samples[i] = distribution(generator);
   }
@@ -52,7 +59,7 @@ std::vector<T> DataSample(const std::vector<T> &data, const int k) {
   return sampled_data;
 }
 
-// Discretesample draws k samples sampled at random according to distribution
+// DiscreteSample draws k samples sampled at random according to distribution
 // `prob` with replacement, where `prob` is a probability array whose elements
 // sum to 1. T is one of { float, double }.
 template <typename T>
@@ -63,11 +70,12 @@ std::vector<int> DiscreteSample(const std::vector<T> &prob, const int k) {
   assert(N > 1);
   assert(k != 0);
 
-  // Check that the sum of the probabilities is equal to 1.
+  // Check that the sum of the probabilities is equal to 1 (accommodating
+  // rounding errors).
   T prob_sum = 0;
   for (auto &p : prob)
     prob_sum += p;
-  assert(std::fabs(prob_sum - 1.0) < std::numeric_limits<T>::min());
+  assert(std::abs(prob_sum - 1.0) < kSufficientlySmallFloat);
 
   // Create index vector.
   std::vector<int> idx;
@@ -89,4 +97,81 @@ std::vector<int> DiscreteSample(const std::vector<T> &prob, const int k) {
   }
   return sampled_idx;
 }
+
+// GaussianPdf computes the probability density for a sample `x`, according to
+// mean `mean` and covariance `sigma`.
+// Inputs:
+//  x:      vector of size M
+//  mean:   vector of size M
+//  sigma:  matrix of size MxM
+template <typename T>
+T GaussianPdf(const VecXt<T> &x, const VecXt<T> &mean, const MatXt<T> &sigma) {
+  assert(x.rows() == mean.rows());
+  assert(x.cols() == mean.cols());
+  assert(sigma.rows() == sigma.cols());
+
+  // Precompute constants.
+  int n = std::max(x.rows(), x.cols());
+  const T log_sqrt_2pi = 0.5 * std::log(2 * M_PI);
+
+  // Use Cholesky decomposition so as to prevent numerical instability in
+  // computing the inverse and determinant.
+  Eigen::LLT<MatXt<T>> chol(sigma);
+
+  if (chol.info() == Eigen::NumericalIssue)
+    throw std::runtime_error("Possibly non PSD matrix!");
+
+  const typename Eigen::LLT<MatXt<T>>::Traits::MatrixL &L = chol.matrixL();
+  T dist = (L.solve(x - mean)).squaredNorm();
+  return std::exp(-n * log_sqrt_2pi - 0.5 * dist) / L.determinant();
 }
+
+// Covariance computes the sample covariance for an Eigen Matrix `samples` of
+// size MxN, and outputs an Eigen matrix of size MxM.
+// `samples` contains N samples of size M.
+template <typename T> MatXt<T> Covariance(const MatXt<T> &samples) {
+  VecXt<T> mean = samples.rowwise().mean();
+  return Covariance(samples, mean);
+}
+
+// Covariance computes the sample covariance for an Eigen Matrix `samples` of
+// size MxN according to the mean `mean`, and outputs an Eigen matrix of size
+// MxM. `samples` contains N samples of size M.
+template <typename T>
+MatXt<T> Covariance(const MatXt<T> &samples, const VecXt<T> &mean) {
+  int N = samples.cols();
+  int sample_dim = mean.size();
+  assert(samples.rows() == sample_dim);
+
+  MatXt<T> centered = samples.colwise() - mean;
+  MatXt<T> cov = (centered * centered.transpose()) / N;
+
+  // Check that the sizes are correct.
+  assert(cov.rows() == sample_dim);
+  assert(cov.cols() == sample_dim);
+  return cov;
+}
+
+// Covariance computes the weighted covariance for an Eigen Matrix `samples` of
+// size MxN according to the mean `mean`, and outputs an Eigen matrix of size
+// MxM. `samples` contains N samples of size M.
+template <typename T>
+MatXt<T> Covariance(const MatXt<T> &samples, const VecXt<T> &mean,
+                    const VecXt<T> &weights) {
+
+  int N = samples.cols();
+  int sample_dim = mean.size();
+  assert(samples.rows() == sample_dim);
+  assert(weights.size() == N);
+
+  MatXt<T> centered = samples.colwise() - mean;
+  MatXt<T> weighted_centered = centered.array().rowwise() * weights.transpose().array();
+  MatXt<T> cov = (centered * weighted_centered.transpose()) / weights.sum();
+
+  // Check that the sizes are correct.
+  assert(cov.rows() == sample_dim);
+  assert(cov.cols() == sample_dim);
+  return cov;
+}
+
+} // namespace stats_utils
